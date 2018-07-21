5b7249adcb9abc719d13454f604606d4283f0e7b
john smith <shukrun.sahar@example.com>
2018-07-21 14:59:56 +0300

    Add command routing

--- server.py
+++ server.py
@@ -1,12 +1,14 @@
 import socket
+import json
+import sys
 
 PORT_NUM = 5555
 
-COMMANDS = {'help':['help'],
-            'cmd':['cmd [OPTION] OPTION - console command'],
-            'getfile':['getfile [FILEPATH] [LOCALPATH]- get file from remote'],
-            'sendfile':['sendfile [LOCALPATH] [REMOTEPATH]- send file to remote'],
-            'exit':['stop server'],
+COMMANDS = {'help':'print help',
+            'cmd':'cmd [OPTION] OPTION - console command',
+            'getfile':'getfile [FILEPATH] [LOCALPATH]- get file from remote',
+            'sendfile':'sendfile [LOCALPATH] [REMOTEPATH]- send file to remote',
+            'exit':'stop server',
            }
 
 class Server(object):
@@ -28,6 +30,25 @@ class Server(object):
         command = 'initial'
         while command not in 'exit':
             command = raw_input('reverse_shell:{}>'.format(address))
+            
+            if 'help' in command:
+                print json.dumps(COMMANDS, indent=4)
+
+            if 'exit' in command:
+                print 'exit server'
+                sys.exit()
+
+            if 'cmd' in command:
+                self.excuteRemoteCommand(client, address, command)
+            
+            if 'getfile' in command:
+                print 'getfile'
+            
+            if 'sendfile' in command:
+                print 'sendfile'
+
+    def excuteRemoteCommand(self,client, address, command):
+        print 'excutecommand'
 
 if __name__ == "__main__":
     Server('',PORT_NUM).listen()
