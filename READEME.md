64188ff3ab0a969d45f914e055f429400736f255
john smith <shukrun.sahar@example.com>
2018-07-27 21:55:27 +0300

    Edit readme.md

--- READEME.md
+++ READEME.md
@@ -1,1 +1,2 @@
-#reveseshell
+# Reverse shell
+This is simple implementation of revese shell written in python.
--- file_util.py
+++ file_util.py
@@ -15,11 +15,3 @@ def createFileWithData(path, data):
         return "Success create file {}".format(path)
     except:
         return "Failed to create file {}".format(path)
-
-
-
-# testing:
-# path = os.path.expanduser('~/theWorker123.txt')
-# data = getFileData(path)
-# print data
-#createFileWithData('/home/john/test1.txt',data)
